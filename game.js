d57a44d79afcfacf22777d778b061edd520e5ff7
FedericoB <federicob@gpl.com.ar>
2020-11-10 18:26:39 -0300

    using keyboard

--- game.js
+++ game.js
@@ -1,5 +1,16 @@
 var canvas = null, ctx = null;
 var x = 50, y = 50;
+var lastPress = null;
+var dir = 0;
+var pause = true;
+
+const KEY_LEFT = 37,
+    KEY_UP = 38,
+    KEY_RIGHT = 39,
+    KEY_DOWN = 40,
+    KEY_ENTER = 13;
+
+
 
 function paint(ctx) {
     ctx.fillStyle = '#000';
@@ -7,25 +18,87 @@ function paint(ctx) {
 
     ctx.fillStyle = '#0f0';
     ctx.fillRect(x, y, 10, 10);
-}
 
-function act(){
-    x += 2;
-    if (x > canvas.width) {
-        x = 0;
+    // Draw pause
+    if (pause) {
+        ctx.textAlign = 'center';
+        ctx.fillText('PAUSE', 150, 75);
+        ctx.textAlign = 'left';
     }
 }
 
-function run() {
-    window.requestAnimationFrame(run);
-    act();
+function act(){
+    if(!pause){
+
+        // Change Direction
+        if (lastPress == KEY_UP) {
+            dir = 0;
+        }
+        if (lastPress == KEY_RIGHT) {
+            dir = 1;
+        }
+        if (lastPress == KEY_DOWN) {
+            dir = 2;
+        }
+        if (lastPress == KEY_LEFT) {
+            dir = 3;
+        }
+        // Move Rect
+        if (dir == 0) {
+            y -= 10;
+        }
+        if (dir == 1) {
+            x += 10;
+        }
+        if (dir == 2) {
+            y += 10;
+        }
+        if (dir == 3) {
+            x -= 10;
+        }
+        
+        //Jump sides
+        if (x > canvas.width) {
+            x = 0;
+        }
+        if (y > canvas.height) {
+            y = 0;
+        }
+        if (x < 0) {
+            x = canvas.width;
+        }
+        if (y < 0) {
+            y = canvas.height;
+        }
+    }
+
+    // Pause/Unpause
+    if (lastPress == KEY_ENTER) {
+        pause = !pause;
+        lastPress = null;
+    }
+}
+
+function repaint() {
+    window.requestAnimationFrame(repaint);
     paint(ctx);
+}
+
+function run() {
+    setTimeout(run, 50);
+    act();
 }
 
 function init() {
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
     run();
+    repaint();
 }
 
 window.addEventListener('load', init, false);
+
+document.addEventListener('keydown', function (evt) {
+    lastPress = evt.which;
+    //evt.key
+}, false);
